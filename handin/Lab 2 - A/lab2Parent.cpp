284597811c3a2d87b51e1f5e7cd399e9663d4244
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2020-09-27 18:15:22 -0700

    added handin folder

---
+++ handin/Lab 2 - A/lab2Parent.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "../../rt.h"
+
+using namespace std;
+
+int main() {
+	cout << "Creating 3 Child Processes\n";
+
+	CProcess child1("C:\\Users\\Isabelle\\Documents\\UBC_ELEC\\CPEN333\\Labs\\lab2\\lab2\\Debug\\lab2Child1.exe",
+		NORMAL_PRIORITY_CLASS,
+		OWN_WINDOW,
+		ACTIVE
+	);
+
+	CProcess child2("C:\\Users\\Isabelle\\Documents\\UBC_ELEC\\CPEN333\\Labs\\lab2\\lab2\\Debug\\lab2Child2.exe",
+		NORMAL_PRIORITY_CLASS,
+		OWN_WINDOW,
+		ACTIVE
+	);
+
+	CProcess child3("C:\\Users\\Isabelle\\Documents\\UBC_ELEC\\CPEN333\\Labs\\lab2\\lab2\\Debug\\lab2Child3.exe",
+		NORMAL_PRIORITY_CLASS,
+		OWN_WINDOW,
+		ACTIVE
+	);
+
+	cout << "Child Processes Activated\n";
+
+	cout << "Waiting for Child1 to terminate\n";
+	child1.WaitForProcess();
+
+	cout << "Waiting for Child2 to terminate\n";
+	child2.WaitForProcess();
+
+	cout << "Waiting for Child3 to terminate\n";
+	child3.WaitForProcess();
+
+	cout << "All processes have ended";
+
+	return 0;
+}
