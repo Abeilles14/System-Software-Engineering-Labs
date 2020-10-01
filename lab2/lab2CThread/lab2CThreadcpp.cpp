8d669a398cb662435218c0fc6508c74f7fc6c700
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2020-09-30 18:10:10 -0700

    part 2 done

--- lab2/lab2CThread/lab2CThreadcpp.cpp
+++ lab2/lab2CThread/lab2CThreadcpp.cpp
@@ -1,30 +1,62 @@
-#include "../../rt.h"
+#include <iostream>
+#include <windows.h>
+#include <process.h>
+
+#define UINT unsigned int
 
 UINT __stdcall ChildThread1(void* args);
 UINT __stdcall ChildThread2(void* args);
 
+using namespace std;
+
 struct threadInfo {
-	std::string threadName;
+	string threadName;
 	int iterations;
-	std::string threadMessage;
+	string threadMessage;
 };
 
 int main() {
 	threadInfo thread1;
+	UINT thread1ID = 0;
 	thread1.threadName = "First thread";
 	thread1.iterations = 100;
 	thread1.threadMessage = "Hello from thread 1\n";
 
 	threadInfo thread2;
+	UINT thread2ID = 0;
 	thread2.threadName = "Second thread";
 	thread2.iterations = 200;
 	thread2.threadMessage = "Hello from thread 2\n";
 
-	CThread t1(ChildThread1, ACTIVE, &thread1);
-	CThread t2(ChildThread2, ACTIVE, &thread2);
+	// Thread setup
+	UINT flags = NORMAL_PRIORITY_CLASS; // Priority,
 
-	t1.WaitForThread();
-	t2.WaitForThread();
+	HANDLE ThreadHandle1 = (HANDLE)(_beginthreadex(NULL, 0, ChildThread1, &thread1, flags, &thread1ID));
+	if (!ThreadHandle1) {
+		cout << "Unable to Create Thread";
+		return 0;
+	}
+
+	Sleep(100);
+
+	HANDLE ThreadHandle2 = (HANDLE)(_beginthreadex(NULL, 0, ChildThread2, &thread2, flags, &thread2ID));
+	if (!ThreadHandle2) {
+		cout << "Unable to Create Thread";
+		return 0;
+	}
+	Sleep(100);
+
+	cout << "Waiting for thread 1 to finish\n";
+	if (WaitForSingleObject(ThreadHandle1, INFINITE) == WAIT_FAILED) {
+		cout << "Unable to Wait for Thread 1";
+		return 0;
+	}
+
+	cout << "Waiting for thread 2 to finish\n";
+	if (WaitForSingleObject(ThreadHandle2, INFINITE) == WAIT_FAILED) {
+		cout << "Unable to Wait for Thread 2";
+		return 0;
+	}
 
 	return 0;
 }
