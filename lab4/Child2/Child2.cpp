a7f874801ef58ab165d550b49f8bc3c9c98c3fd1
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2020-10-06 18:46:07 -0700

    lab4 A done

--- lab4/Child2/Child2.cpp
+++ lab4/Child2/Child2.cpp
@@ -15,15 +15,15 @@ int main() {
 
 	int x;
 
-	printf("Child Process 1 Opening Pipeline 1\n");
-	CPipe p1("MyPipe1", 1024);		// create pipes size 1024
+	printf("Child Process 2 Opening Pipeline 2\n");
+	CPipe p1("MyPipe2", 1024);		// create pipes size 1024
 
 	p1.Read(&i, sizeof(i));			// read int i from pipe and store value 
-	cout << "Child Process 1 read integer = " << i << " from Pipeline 1\n";
+	cout << "Child Process 2 read integer = " << i << " from Pipeline 2\n";
 
 	p1.Read(&array1[0], sizeof(array1));		// read array of integers and store values
-	
-	printf("Child Process 1 read array = ");
+
+	printf("Child Process 2 read array = ");
 	for (x = 0; x < sizeof(array1) / sizeof(array1[0]); x++) {
 		cout << " " << array1[x];
 	}
@@ -31,12 +31,12 @@ int main() {
 	putchar('\n');
 
 	p1.Read(&name[0], sizeof(name));		// read string frome pipe and store values
-	cout << "Child Process 1 read string = " << name << ", from Pipeline 1\n";
+	cout << "Child Process 2 read string = " << name << ", from Pipeline 2\n";
 
 	p1.Read(&mystruct, sizeof(mystruct));
-	cout << "Child Process 1 read [" << mystruct.x << "," << mystruct.y << "] from Pipeline 1\n";
+	cout << "Child Process 2 read [" << mystruct.x << "," << mystruct.y << "] from Pipeline 2\n";
 
-	Sleep(5000);
+	Sleep(4000);
 
 	return 0;
 }
