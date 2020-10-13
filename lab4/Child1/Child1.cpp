b1708bed58747d10fdef0bf741b738d6043a8e28
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2020-10-12 18:16:23 -0700

    lab5 part b atomic flag

--- lab4/Child1/Child1.cpp
+++ lab4/Child1/Child1.cpp
@@ -1,27 +1,182 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <stdio.h>
+#include <winsock.h>
 
-#include <winsock2.h>
-#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib")		//link with ws2_32.lib library
 
-int main() {
-	WSADATA WSAData;
-	SOCKET sock;
-	SOCKADDR_IN sin;
+void StreamServer(short nPort);
 
-	char buffer[225];
+#define PRINTERROR(s) fprintf(stderr,"\n%: %d\n", s, WSAGetLastError())		// Helper macro for displaying errors
 
-	WSAStartup(MAKEWORD(2, 0), &WSAData);		// set up to connect on IRC
+int main(int argc, char** argv) {
+	WORD wVersionRequested = MAKEWORD(1, 1);
+	WSADATA wsaData;
+	int nRet;
+	short nPort;
 
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	sin.sin_addr.s_addr = inet_addr("62.250.14.6");
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(6667);
+	if (argc != 2)				// check port arg
+	{
+		fprintf(stderr, "\nSyntax: server PortNumber\n");
+		return 0;
+	}
 
-	connect(sock, (SOCKADDR*)& sin, sizeof(sin));
-	recv(sock, buffer, sizeof(buffer), 0);
+	nPort = atoi(argv[1]);
 
-	closesocket(sock);
+	nRet = WSAStartup(wVersionRequested, &wsaData);		// init winsock and chek version
+	if (wsaData.wVersion != wVersionRequested)
+	{
+		fprintf(stderr, "\n Wrong version\n");
+		return 0;
+	}
+
+
+	//
+	// Do the stuff a stream server does
+	//
+	StreamServer(nPort);
+
+
+	//
+	// Release WinSock
+	//
 	WSACleanup();
 
 	return 0;
+}
+
+void StreamServer(short nPort)
+{
+	//
+	// Create a TCP/IP stream socket to "listen" with
+	//
+	SOCKET	listenSocket;
+
+	listenSocket = socket(AF_INET,			// Address family
+		SOCK_STREAM,		// Socket type
+		IPPROTO_TCP);		// Protocol
+	if (listenSocket == INVALID_SOCKET)
+	{
+		PRINTERROR("socket()");
+		return;
+	}
+
+
+	//
+	// Fill in the address structure
+	//
+	SOCKADDR_IN saServer;
+
+	saServer.sin_family = AF_INET;
+	saServer.sin_addr.s_addr = INADDR_ANY;	// Let WinSock supply address
+	saServer.sin_port = htons(nPort);		// Use port from command line
+
+	//
+	// bind the name to the socket
+	//
+	int nRet;
+
+	nRet = bind(listenSocket,	// Socket 
+		(LPSOCKADDR)& saServer,	// Our address
+		sizeof(struct sockaddr));// Size of address structure
+	if (nRet == SOCKET_ERROR)
+	{
+		PRINTERROR("bind()");
+		closesocket(listenSocket);
+		return;
+	}
+
+	//
+	// This isn't normally done or required, but in this 
+	// example we're printing out where the server is waiting
+	// so that you can connect the example client.
+	//
+	int nLen;
+	nLen = sizeof(SOCKADDR);
+	char szBuf[256];
+
+	nRet = gethostname(szBuf, sizeof(szBuf));
+	if (nRet == SOCKET_ERROR)
+	{
+		PRINTERROR("gethostname()");
+		closesocket(listenSocket);
+		return;
+	}
+
+	//
+	// Show the server name and port number
+	//
+	printf("\nServer named %s waiting on port %d\n",
+		szBuf, nPort);
+
+	//
+	// Set the socket to listen
+	//
+
+	printf("\nlisten()");
+	nRet = listen(listenSocket,	// Bound socket
+		SOMAXCONN);	// Number of connection request queue
+	if (nRet == SOCKET_ERROR)
+	{
+		PRINTERROR("listen()");
+		closesocket(listenSocket);
+		return;
+	}
+
+	//
+	// Wait for an incoming request
+	//
+	SOCKET	remoteSocket;
+
+	printf("\nBlocking at accept()");
+	remoteSocket = accept(listenSocket,  // Listening socket
+		NULL,	// Optional client address
+		NULL);
+	if (remoteSocket == INVALID_SOCKET)
+	{
+		PRINTERROR("accept()");
+		closesocket(listenSocket);
+		return;
+	}
+
+	//
+	// We're connected to a client
+	// New socket descriptor returned already
+	// has clients address
+
+	//
+	// Receive data from the client
+	//
+	memset(szBuf, 0, sizeof(szBuf));
+	nRet = recv(remoteSocket,					// Connected client
+		szBuf,							// Receive buffer
+		sizeof(szBuf),					// Length of buffer
+		0);								// Flags
+	if (nRet == INVALID_SOCKET)
+	{
+		PRINTERROR("recv()");
+		closesocket(listenSocket);
+		closesocket(remoteSocket);
+		return;
+	}
+
+	//
+	// Display received data
+	//
+	szBuf[nRet] = 0;
+	printf("\nData received: %s", szBuf);
+
+	//
+	// Send data back to the client
+	//
+	strcpy_s(szBuf, "From the Server");
+	nRet = send(remoteSocket,				// Connected socket
+		szBuf,						// Data buffer
+		strlen(szBuf),				// Lenght of data
+		0);							// Flags
+
+//
+// Close BOTH sockets before exiting
+//
+	closesocket(remoteSocket);
+	closesocket(listenSocket);
+	return;
 }
--- lab4/lab4/lab4.cpp
+++ lab4/lab4/lab4.cpp
@@ -1,40 +1,157 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-
 #include <stdio.h>
-#include <winsock2.h>
+#include <winsock.h>
 
-#pragma comment(lib, "ws2_32.lib")		// winsock library
+#pragma comment(lib, "ws2_32.lib")		//link with ws2_32.lib library
 
-int main() {
-	WSADATA WSAData;
-	SOCKET sock;
-	SOCKET csock = 0;
-	SOCKADDR_IN sin;
-	SOCKADDR_IN csin;
+// Function prototype
+void StreamClient(char* szServer, short nPort);
 
-	WSAStartup(MAKEWORD(2, 0), &WSAData);
+// Helper macro for displaying errors
+#define PRINTERROR(s) fprintf(stderr,"\n%: %d\n", s, WSAGetLastError())
 
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	sin.sin_addr.s_addr = INADDR_ANY;
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(23);
-	bind(sock, (SOCKADDR*)& sin, sizeof(sin));
+int main(int argc, char** argv) {
+	WORD wVersionRequested = MAKEWORD(1, 1);
+	WSADATA wsaData;
+	int nRet;
+	short nPort;
 
-	listen(sock, 0);
-	int val = 0;
-	
-	while (1) {
-		int sizeof_csin = sizeof(csin);
-		val = accept(sock, (SOCKADDR*)& csin, &sizeof_csin);
-
-		if (val != INVALID_SOCKET) {
-			send(csock, "Hello world!\r\n", 14, 0);
-			closesocket(csock);
-		}
+	//
+	// Check for the host and port arguments
+	//
+	if (argc != 3)
+	{
+		fprintf(stderr, "\nSyntax: client ServerName PortNumber\n");
+		return 0;
 	}
 
-	closesocket(sock);
-	WSACleanup();		// cleanup sockets
+	nPort = atoi(argv[2]);
+
+
+	//
+	// Initialize WinSock and check the version
+	//
+	nRet = WSAStartup(wVersionRequested, &wsaData);
+	if (wsaData.wVersion != wVersionRequested)
+	{
+		fprintf(stderr, "\n Wrong version\n");
+		return 0;
+	}
+
+
+	//
+	// Go do the stuff a stream client does
+	//
+	StreamClient(argv[1], nPort);
+
+
+	//
+	// Release WinSock
+	//
+	WSACleanup();
 
 	return 0;
+}
+
+void StreamClient(char* szServer, short nPort)
+{
+	printf("\nStream Client connecting to server: %s on port: %d",
+		szServer, nPort);
+
+	//
+	// Find the server
+	//
+	LPHOSTENT lpHostEntry;
+
+	lpHostEntry = gethostbyname(szServer);
+	if (lpHostEntry == NULL)
+	{
+		PRINTERROR("gethostbyname()");
+		return;
+	}
+
+	//
+	// Create a TCP/IP stream socket
+	//
+	SOCKET	theSocket;
+	
+	theSocket = socket(AF_INET,// Address family
+		SOCK_STREAM,// Socket type
+		IPPROTO_TCP);		// Protocol
+	if (theSocket == INVALID_SOCKET)
+	{
+		PRINTERROR("socket()");
+		return;
+	}
+
+	//
+	// Fill in the address structure
+	//
+	SOCKADDR_IN saServer;
+
+	saServer.sin_family = AF_INET;
+
+	// Server's address
+	saServer.sin_addr = *((LPIN_ADDR)* lpHostEntry->h_addr_list);
+	saServer.sin_port = htons(nPort);	// Port number from command line
+
+	//
+	// connect to the server
+	//
+	int nRet;
+
+	nRet = connect(theSocket,	// Socket
+		(LPSOCKADDR)& saServer,	// Server address
+		sizeof(struct sockaddr));// Length of server address structure
+	if (nRet == SOCKET_ERROR)
+	{
+		PRINTERROR("socket()");
+		closesocket(theSocket);
+		return;
+	}
+
+
+	//
+	// Send data to the server
+	//
+	char szBuf[256];
+
+	strcpy_s(szBuf, "From the Client");
+	nRet = send(theSocket,// Connected socket
+		szBuf,	// Data buffer
+		strlen(szBuf),	// Length of data
+		0);	// Flags
+	if (nRet == SOCKET_ERROR)
+	{
+		PRINTERROR("send()");
+		closesocket(theSocket);
+		return;
+	}
+
+
+	//
+	// Wait for a reply
+	//
+	nRet = recv(theSocket, // Connected socket
+		szBuf,	// Receive buffer
+		sizeof(szBuf),	// Size of receive buffer
+		0);	// Flags
+	if (nRet == SOCKET_ERROR)
+	{
+		PRINTERROR("recv()");
+		closesocket(theSocket);
+		return;
+	}
+
+
+	//
+	// Display the received data
+	//
+
+	szBuf[nRet] = 0;
+	printf("\nData received: %s", szBuf);
+
+
+	closesocket(theSocket);
+
+	return;
 }
